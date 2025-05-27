12783a629d1eb61fc735501b5a0cea299d5ea2f2
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-05-27 16:32:15 +0700

    Menambahkan Singleton Pattern pada conn.php untuk user

--- tukubuku.com/Connection/conn.php
+++ tukubuku.com/Connection/conn.php
@@ -1,10 +1,67 @@
-<?php 
- 
-$db = mysqli_connect("localhost","root","","gigavalboook");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+<?php
+
+class Database {
+    private static $instance = null; // Menyimpan satu-satunya instance koneksi
+    private $connection; // Menyimpan objek koneksi MySQLi
+
+    // Konstruktor disembunyikan (private) agar tidak bisa diinstansiasi langsung dari luar
+    private function __construct() {
+        // Detail koneksi database
+        $host = "localhost";
+        $username = "root";
+        $password = ""; // Kosong jika tidak ada password
+        $database = "fasilkomlib";
+
+        // Buat koneksi MySQLi
+        $this->connection = new mysqli($host, $username, $password, $database);
+
+        // Periksa koneksi
+        if ($this->connection->connect_error) {
+            die("Koneksi database gagal: " . $this->connection->connect_error);
+        }
+        // Opsional: Atur charset jika diperlukan, contoh:
+        // $this->connection->set_charset("utf8mb4");
+    }
+
+    // Metode untuk mendapatkan satu-satunya instance koneksi
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Database(); // Buat instance baru jika belum ada
+        }
+        return self::$instance;
+    }
+
+    // Metode untuk mendapatkan objek koneksi MySQLi yang sebenarnya
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    // Metode disembunyikan (private) agar instance tidak bisa di-clone
+    private function __clone() {}
+
+    // Metode disembunyikan (private) agar instance tidak bisa di-unserialize
+    private function __wakeup() {}
 }
- 
+
+// Cara penggunaan:
+// Untuk mendapatkan instance Database (dan koneksi):
+$dbInstance = Database::getInstance();
+
+// Untuk mendapatkan objek koneksi MySQLi yang sebenarnya untuk query:
+$koneksi = $dbInstance->getConnection();
+
+// Contoh penggunaan koneksi:
+// $query = "SELECT * FROM users";
+// $result = $koneksi->query($query);
+// if ($result) {
+//     while ($row = $result->fetch_assoc()) {
+//         echo $row['username'] . "<br>";
+//     }
+// } else {
+//     echo "Error: " . $koneksi->error;
+// }
+
+// Jangan lupa untuk menutup koneksi jika aplikasi selesai (opsional, PHP akan menutupnya secara otomatis di akhir skrip)
+// $koneksi->close();
+
 ?>
